6bfdfac4f54e6847b5a11c34d622ecef989d8bc6
anas-ce <anas@cyberevangelists.com>
2024-04-18 18:13:22 +0500

    credit with mongodb

--- app/credits.py
+++ app/credits.py
@@ -1,17 +1,28 @@
-credits = 1
-
-def check_credits():
-    print('hi')
-    global credits
-    if credits > 0:
-        return True
+from pymongo import MongoClient
+import os
+mongo_client = MongoClient(os.environ.get('URI'))
+db = mongo_client['openAI_PDF']
+users_collection = db["users"]
+def check_credits(mail):
+    user = users_collection.find_one({"email": mail})  
+    if user:
+        current_credits = user.get("credits", 0) 
+        if current_credits > 0:
+            return True
+        else:
+            return False
     else:
         return False
-
-def update_credits():
-    global credits
-    if credits > 0:
-        credits -= 1
-        return True
+def update_credit(mail):
+    user = users_collection.find_one({"email": mail})  
+    if user:
+        current_credits = user.get("credits", 0) 
+        if current_credits > 0:
+            new_credits = current_credits - 1
+            users_collection.update_one({"email": mail}, {"$set": {"credits": new_credits}})
+        else:
+           return("You doont have enough credits")
     else:
         return False
+    
+    
